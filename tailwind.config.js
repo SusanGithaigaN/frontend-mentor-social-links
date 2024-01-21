b590edf3cfff8543423d147e7e3707ef5f003d87
susangithaiga <susangithaiga26@gmail.com>
2024-01-21 12:07:05 +0300

    Complete

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  colors: () =>({
+    darkblack: "hsl(0, 0%, 20%)",
+  }),
+  plugins: [],
+  
+}
